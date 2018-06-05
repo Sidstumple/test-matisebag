6b85a2390374c5293fae2fc36b88feb7d484e32e
denniswegereef <denniswegereef@gmail.com>
2018-06-05 13:40:01 +0200

    After testing

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,43 @@
 <template>
 	<section class="container">
 		<div>
+			<form action="">
+			<field-input
 
+	input-type="radio"
+	input-name="radio-test"
+	input-id="radio 1"
+	label-text="Radio button 1"
+	aria-label="Radio button component code example"/>
 
+	<field-input
+input-type="radio"
+input-name="radio-test"
+input-id="radio 2"
+label-text="Radio button 2"
+aria-label="Radio button component code example"/>
+
+<field-input
+input-type="radio"
+input-name="radio-test"
+input-id="radio 3"
+label-text="Radio button 3"
+aria-label="Radio button component code example"/>
+
+<field-input
+	input-type="text"
+	input-name="example"
+	input-id="text"
+	label-text="Example"
+	aria-label="Text input component example"/>
+</form>
+
+<type-writer
+	heading="HGAHA IK BEN DNENIS!!!"
+	:wait="showTyping"
+	sub="Gekke componenten enzo "
+	@doneTyping="show = true">
+</type-writer>
 		</div>
 	</section>
 </template>
@@ -14,5 +49,8 @@ export default {
 </script>
 
 <style>
-
+.typewriter__title h1 {
+	font-size: 1.4rem;
+	color: hotpink;
+}
 </style>
