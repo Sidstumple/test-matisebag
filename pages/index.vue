6dfdbe90095021c7c874bf7f67d49d6c82397925
seb <seb@matise.nl>
2018-06-05 14:46:38 +0200

    seb’s first steps in the bag

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,33 @@
 <template>
 	<section class="container">
 		<div>
+			<type-writer
+				heading="Cyd is gek"
+				:wait="showTyping"
+				sub="Hahahahahah"
+				@doneTyping="show = true">
+			</type-writer>
 
-
+			<form>
+				<field-input
+					input-type="radio"
+					input-name="radio-test"
+					input-id="radio"
+					label-text="Radio button"
+					aria-label="Radio button component code example"/>
+				<field-input
+					input-type="radio"
+					input-name="radio-test"
+					input-id="radio2"
+					label-text="Radio button2"
+					aria-label="Radio button component code example2"/>
+				<field-input
+					input-type="text"
+					input-name="example"
+					input-id="text1"
+					label-text="Cyd is gek"
+					aria-label="Ik weet eigenlijk niet waar dit te zien gaat zijn"/>
+			</form>
 		</div>
 	</section>
 </template>
---
+++ plugins/dynamic-components.js
@@ -0,0 +1,7 @@
+import Vue from 'vue';
+	import {TypeWriter, Logo, FieldInput} from 'matise-assets';
+
+	Vue.component('type-writer', TypeWriter);
+	Vue.component('logo', Logo);
+	Vue.component('field-input', FieldInput);
+	Vue.use(TypeWriter, Logo, FieldInput);
